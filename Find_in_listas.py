54decae30f87f554078e26bfedee13dbb49c868f
Luis Osuna <luisfernandoosal@gmail.com>
2025-05-12 22:46:33 -0700

    12-05-25 22:46

---
+++ Find_in_listas.py
@@ -0,0 +1,5 @@
+frutas = ['Mango', 'Uva', 'Pera'] # Con el metodo index, buscara en que indice se encuentra el string que le indiquemos entre parentesis.
+print(frutas.index('Uva'))
+
+frutas = ['Mango', 'Uva', 'Pera', 'Uva'] # Y con el metodo count, contara cuantas veces aparece
+print(frutas.count('Uva'))
--- IteracionListas.py
+++ IteracionListas.py
@@ -1,7 +1,24 @@
 frutas = ['Mango', 'Fresa', 'Uva']
 
-for fruta in frutas:
+for fruta in frutas: # En este solo iteraremos todos los elementos de la lista
     print(fruta)
 
-for fruta in enumerate(frutas):
+for fruta in enumerate(frutas): # En este nos iterara los elementos de la lista pero le agregara un indice a cada elemento
     print(fruta)
+
+for indice, fruta in enumerate(frutas): # En el ejemplo anterior nos imprimia los elemntos en tuplas. Si queremos que los imprima, debemos crear un elemento mas
+    # en la cabecera del for para que a cada uno se le asigne un elemento, por lo cual el primer elemento tomara el valor del indice y el segundo el de la fruta.
+    print(indice, fruta)
+
+for indice, fruta in enumerate(frutas): # En este solo indicaremos que se imprima el segundo elemento de la dupla, el cual sera la fruta.
+    print(fruta)
+
+for indice, fruta in enumerate(frutas): # En este solo sera imprimir el indice.
+    print(indice)
+
+for fruta in enumerate(frutas): # Tambien podemos usar corchetes para indicar el indice de la tupla y que imprima ese elemento, en este caso 0 tomara el elemento del
+    # indice de las frutas
+    print(fruta[0])
+
+for fruta in enumerate(frutas): # Y en este ultimo tomara las frutas.
+    print(fruta[1])
